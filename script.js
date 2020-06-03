c1f92d8bd3f5a52063c1a4109eb8ad2e83a5dfe8
makushu <phatumakushu@gmail.com>
2020-06-03 17:17:39 +0200

    colors added

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 
-function changeMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+
+
+
+$(document).ready(function(){
+  $(".darkMode").toggle(
+    function(){$(".likes-group").css({"background-color": "#001a33"});},
+    function(){$(".likes-group").css({"background-color": "#ccccff"});
+   
+  });
+});
+
+
+$(document).ready(function(){
+  $(".darkMode").toggle(
+    function(){$(".followers-group").css({"background-color": "#001a33"});},
+    function(){$(".followers-group").css({"background-color": "#ccccff"});
+  });
+});
+
+
+$(document).ready(function(){
+  $(".darkMode").toggle(
+    function(){$("body").css({"background-color": "#000066"});},
+    function(){$("body").css({"background-color": "#ffffff"});
+    
+  });
+});
+
+
